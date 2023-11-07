dcc1f449d97c4edf0728ed081799947be49b7757
Henrique Fan <fanhenrique@gmail.com>
2023-11-07 17:01:32 -0300

    rgba

---
+++ include/rgba.h
@@ -0,0 +1,10 @@
+#ifndef RGBA_H
+#define RGBA_H
+class RGBA
+{
+public:
+  float r, g, b, a;
+  RGBA();
+  RGBA(float r, float g, float b, float a);
+};
+#endif
---
+++ rgba.cpp
@@ -0,0 +1,12 @@
+// my imports
+#include "include/rgba.h"
+
+RGBA::RGBA() {}
+
+RGBA::RGBA(float r, float g, float b, float a)
+{
+  this->r = r;
+  this->g = g;
+  this->b = b;
+  this->a = a;
+};
