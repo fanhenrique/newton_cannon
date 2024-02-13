644626a486953640062a43f67d9352f59017bc55
Henrique Fan <fanhenrique@gmail.com>
2024-02-13 18:25:58 -0300

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 #include <vector>
 #include <math.h>
 #include <string>
@@ -23,6 +24,8 @@ using namespace std;
 int main(int argc, char **argv)
 {
 
+  srand(time(NULL));
+
   auto [display, timer, event, queue, camera, camera_informations, font] = init();
 
   ALLEGRO_BITMAP *atmo_sprite = al_load_bitmap("./images/atmo.png");
@@ -38,27 +41,25 @@ int main(int argc, char **argv)
 
   cout << "window " << SCREEN_WIDTH << " " << SCREEN_HEIGHT << endl;
 
-  Body *earth = new Body({SCREEN_WIDTH / 2.f, SCREEN_HEIGHT / 2.f}, 270.0, 22000.0, {0.05, 0.6, 0.8, 1.0});
+  Body *earth = new Body({SCREEN_WIDTH / 2.f, SCREEN_HEIGHT / 2.f}, 300.f, 20000.f, {0.8, 0.2, 0.2, 1.0});
 
   // TODO spin earth
   // vector<ALLEGRO_BITMAP *> *earth_sprites = load_bitmap();
 
   vector<Projectile *> *projectiles = new vector<Projectile *>();
-  // for (int i = 0; i < 1; i++)
-  // {
-  Projectile *p0 = new Projectile({earth->position.x, earth->position.y - earth->radius - 120}, 20.0, 1.0, {-7.8, 0.0}, {0.8, 0.8, 0.05, 1.0});
-  Projectile *p1 = new Projectile({earth->position.x, earth->position.y - earth->radius - 120}, 20.0, 1.0, {7.8, 0.0}, {0.5, 0.8, 0.7, 1.0});
-  Projectile *p2 = new Projectile({earth->position.x, earth->position.y - earth->radius - 120}, 20.0, 1.0, {-7.5, 0.0}, {0.5, 0.3, 0.5, 1.0});
-  Projectile *p3 = new Projectile({earth->position.x, earth->position.y - earth->radius - 120}, 20.0, 1.0, {7.7, 0.0}, {0.8, 0.8, 0.05, 1.0});
-  Projectile *p4 = new Projectile({earth->position.x, earth->position.y - earth->radius - 120}, 20.0, 1.0, {-8.0, 0.0}, {0.9, 0.8, 0.7, 1.0});
-  Projectile *p5 = new Projectile({earth->position.x, earth->position.y - earth->radius - 120}, 20.0, 1.0, {7.4, 0.0}, {0.6, 0.2, 0.5, 1.0});
-  projectiles->push_back(p0);
-  projectiles->push_back(p1);
-  projectiles->push_back(p2);
-  projectiles->push_back(p3);
-  projectiles->push_back(p4);
-  projectiles->push_back(p5);
-  // }
+  for (int i = 0; i < 3000; i++)
+  {
+    double vx = 5.f + static_cast<double>(rand() / (RAND_MAX / (10.f - 5.f)));
+    double vy = 0.f + static_cast<double>(rand() / (RAND_MAX / (10.f - 0.f)));
+    cout << vx << "," << vy << endl;
+
+    float r = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+    float g = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+    float b = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+
+    Projectile *p = new Projectile({earth->position.x, earth->position.y - earth->radius - 50}, 15.0, 1.0, {vx, vy}, {r, g, b, 1.0});
+    projectiles->push_back(p);
+  }
   // cout << earth_sprites->size() << endl;
 
   al_start_timer(timer);
@@ -94,10 +95,12 @@ int main(int argc, char **argv)
 
       camera_update(&camera, camera_position, x_camera_center, y_camera_center);
 
-      draw_filled_body(earth);
+      // al_draw_filled_rectangle(earth->position.x - earth->radius, earth->position.y - earth->radius, earth->position.x + earth->radius, earth->position.y + earth->radius, {0.6, 0.2, 0.5, 1.0});
 
-      al_draw_scaled_bitmap(atmo_sprite, 0, 0, al_get_bitmap_width(atmo_sprite), al_get_bitmap_height(atmo_sprite), (SCREEN_WIDTH - SCREEN_HEIGHT) / 2, 0, SCREEN_HEIGHT, SCREEN_HEIGHT, 0);
-      al_draw_scaled_bitmap(earth_sprite, 0, 0, al_get_bitmap_width(earth_sprite), al_get_bitmap_height(earth_sprite), (SCREEN_WIDTH - SCREEN_HEIGHT) / 2, 0, SCREEN_HEIGHT, SCREEN_HEIGHT, 0);
+      al_draw_scaled_bitmap(atmo_sprite, 0, 0, al_get_bitmap_width(atmo_sprite), al_get_bitmap_height(atmo_sprite), (earth->position.x - (earth->radius + 100)), (earth->position.y - (earth->radius + 100)), ((earth->radius + 100) * 2), ((earth->radius + 100) * 2), 0);
+      al_draw_scaled_bitmap(earth_sprite, 0, 0, al_get_bitmap_width(earth_sprite), al_get_bitmap_height(earth_sprite), (earth->position.x - earth->radius), (earth->position.y - earth->radius), earth->radius * 2, earth->radius * 2, 0);
+
+      // draw_filled_body(earth);
 
       draw_projectiles(projectiles);
 
@@ -107,7 +110,7 @@ int main(int argc, char **argv)
       draw_camera_cross(x_camera_center, y_camera_center);
 
       // draw acceleration, velocity and position
-      draw_body_informations(font, &camera_informations, p0);
+      draw_body_informations(font, &camera_informations, projectiles->at(0));
 
       al_flip_display();
 
